51c1f4b205f5e03c6ce5a582a125070ade4b6186
marlener87 <53823616+marlener87@users.noreply.github.com>
2023-04-07 17:11:28 +0200

    initialisation projet 6

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +9,19 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        // Ajout code
+        const city = document.createElement('p');
+        city.textContent = city;
+        const country = document.createElement('p');
+        country.textContent = country;
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(city);
+        article.appendChild(country);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, getUserCardDOM }
 }
