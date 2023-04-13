cc903c8af3d6eb5516da94f12f7c006dd74e64b7
marlener87 <53823616+marlener87@users.noreply.github.com>
2023-04-13 18:09:17 +0200

    ajout description des photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,27 +1,56 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
+
+    console.log(data)
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
+
+        // lien d'un photographer => `/photographer.html?id=${id}`
+        // Solution n°1
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
+        // créer lien
+        // lien.setAttribute(...) => ??? recherche google avec "js a set href" 
+
         const h2 = document.createElement( 'h2' );
+        h2.classList.add('title');
         h2.textContent = name;
 
-        // Ajout code
-        const city = document.createElement('p');
-        city.textContent = city;
-        const country = document.createElement('p');
-        country.textContent = country;
+        const cities = document.createElement('h3');
+        cities.textContent = city  + ', ' +  country;
 
+        const description = document.createElement('p');
+        description.textContent = tagline;
+
+        const tjm = document.createElement('p');
+        tjm.textContent = price;
 
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(city);
-        article.appendChild(country);
-        return (article);
+        article.appendChild(cities);
+        article.appendChild(description);
+        article.appendChild(tjm);
+
+
+        // Solution n°2
+        const newArticle = document.createElement( 'article' );
+        newArticle.innerHTML = `
+                <a class="ficheArtiste" href="/photographer.html?id=${id}">
+                    <h2 class="title">${name}</h2>
+                    <h3>${city}, ${country}</h3>
+                    <p>${tagline}</p>
+                    <p>${price}€/jour</p>
+                </a>
+        `
+
+
+        return (newArticle);
     }
-    return { name, picture, city, country, getUserCardDOM }
+    return { name, picture, city, country, tagline, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,57 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+// ...
+const data = {
+    photographer : {
+        "name": "Mimi Keel",
+        "id": 243,
+        "city": "London",
+        "country": "UK",
+        "tagline": "Voir le beau dans le quotidien",
+        "price": 400,
+        "portrait": "MimiKeel.jpg"
+    },
+    media : [
+        {
+			"id": 342550,
+			"photographerId": 82,
+			"title": "Fashion Yellow Beach",
+			"image": "Fashion_Yellow_Beach.jpg",
+			"likes": 62,
+			"date": "2011-12-08",
+			"price": 55
+		},
+		{
+			"id": 8520927,
+			"photographerId": 82,
+			"title": "Fashion Urban Jungle",
+			"image": "Fashion_Urban_Jungle.jpg",
+			"likes": 11,
+			"date": "2011-11-06",
+			"price": 55
+		},
+		{
+			"id": 9025895,
+			"photographerId": 82,
+			"title": "Fashion Pattern on a Pattern",
+			"image": "Fashion_Pattern_on_Pattern.jpg",
+			"likes": 72,
+			"date": "2013-08-12",
+			"price": 55
+		},
+		{
+			"id": 9275938,
+			"photographerId": 82,
+			"title": "Wedding Gazebo",
+			"image": "Event_WeddingGazebo.jpg",
+			"likes": 69,
+			"date": "2018-02-22",
+			"price": 55
+		}
+    ]
+    
+}
+
+
+// data.photographer.name 
+// ...
