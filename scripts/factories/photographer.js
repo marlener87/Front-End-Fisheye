ff19a64103ff6c4151fefc077eda9a73b827d094
marlener87 <53823616+marlener87@users.noreply.github.com>
2023-04-28 17:45:40 +0200

    essaie ajout photos, ne fonctionne pas

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,19 +1,19 @@
 function photographerFactory(data) {
     const { id, name, portrait, city, country, tagline, price } = data;
 
-    console.log(data)
+    //console.log(data)
 
-    const picture = `assets/photographers/${portrait}`;
+    //const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-
-
         // lien d'un photographer => `/photographer.html?id=${id}`
         // Solution n°1
         const article = document.createElement( 'article' );
 
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        //img.setAttribute("src", picture)
+        img.setAttribute("src", portrait)
+        console.log(portrait)
 
         // créer lien
         // lien.setAttribute(...) => ??? recherche google avec "js a set href" 
@@ -31,26 +31,30 @@ function photographerFactory(data) {
         const tjm = document.createElement('p');
         tjm.textContent = price;
 
+        const idPhotographer = document.createElement('p');
+        idPhotographer.textContent = id;
+
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(cities);
         article.appendChild(description);
         article.appendChild(tjm);
-
+        article.appendChild(idPhotographer);
 
         // Solution n°2
         const newArticle = document.createElement( 'article' );
         newArticle.innerHTML = `
                 <a class="ficheArtiste" href="/photographer.html?id=${id}">
+                    <img src=${portrait} >
                     <h2 class="title">${name}</h2>
                     <h3>${city}, ${country}</h3>
                     <p>${tagline}</p>
                     <p>${price}€/jour</p>
+                    <p>id : ${id}</p>
                 </a>
         `
 
-
         return (newArticle);
     }
-    return { name, picture, city, country, tagline, getUserCardDOM }
+    return { name, portrait, city, country, tagline, price, id, getUserCardDOM }
 }
