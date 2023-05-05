fb38ee518c6828c9deaa8fa900418948f7c13d46
marlener87 <53823616+marlener87@users.noreply.github.com>
2023-05-05 17:14:40 +0200

    partie photographers commencée

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -8,52 +8,53 @@ function photographerFactory(data) {
     function getUserCardDOM() {
         // lien d'un photographer => `/photographer.html?id=${id}`
         // Solution n°1
-        const article = document.createElement( 'article' );
+        // const article = document.createElement( 'article' );
 
-        const img = document.createElement( 'img' );
-        //img.setAttribute("src", picture)
-        img.setAttribute("src", portrait)
-        console.log(portrait)
+        // const img = document.createElement( 'img' );
+        // //img.setAttribute("src", picture)
+        // img.setAttribute("src", portrait)
+        // //console.log(portrait)
 
-        // créer lien
-        // lien.setAttribute(...) => ??? recherche google avec "js a set href" 
+        // // créer lien
+        // // lien.setAttribute(...) => ??? recherche google avec "js a set href" 
 
-        const h2 = document.createElement( 'h2' );
-        h2.classList.add('title');
-        h2.textContent = name;
+        // const h2 = document.createElement( 'h2' );
+        // h2.classList.add('title');
+        // h2.textContent = name;
 
-        const cities = document.createElement('h3');
-        cities.textContent = city  + ', ' +  country;
+        // const cities = document.createElement('h3');
+        // cities.textContent = city  + ', ' +  country;
 
-        const description = document.createElement('p');
-        description.textContent = tagline;
+        // const description = document.createElement('p');
+        // description.textContent = tagline;
 
-        const tjm = document.createElement('p');
-        tjm.textContent = price;
+        // const tjm = document.createElement('p');
+        // tjm.textContent = price;
 
-        const idPhotographer = document.createElement('p');
-        idPhotographer.textContent = id;
+        // const idPhotographer = document.createElement('p');
+        // idPhotographer.textContent = id;
 
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(cities);
-        article.appendChild(description);
-        article.appendChild(tjm);
-        article.appendChild(idPhotographer);
+        // article.appendChild(img);
+        // article.appendChild(h2);
+        // article.appendChild(cities);
+        // article.appendChild(description);
+        // article.appendChild(tjm);
+        // article.appendChild(idPhotographer);
 
-        // Solution n°2
+        //Solution n°2
         const newArticle = document.createElement( 'article' );
         newArticle.innerHTML = `
-                <a class="ficheArtiste" href="/photographer.html?id=${id}">
-                    <img src=${portrait} >
-                    <h2 class="title">${name}</h2>
-                    <h3>${city}, ${country}</h3>
-                    <p>${tagline}</p>
-                    <p>${price}€/jour</p>
-                    <p>id : ${id}</p>
-                </a>
+            <a class="ficheArtiste" href="photographer.html?id=${id}">
+                <div class='image'>
+                    <img src=./assets/images/FishEye_Photos/Photographers_ID_Photos/${portrait}>
+                </div>
+                
+                <h2 class="title">${name}</h2>
+                <h3>${city}, ${country}</h3>
+                <p class='tagline'>${tagline}</p>
+                <p class='price'>${price}€/jour</p>
+            </a>
         `
-
         return (newArticle);
     }
     return { name, portrait, city, country, tagline, price, id, getUserCardDOM }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,71 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+const modalbg = document.querySelector("#contact_modal");
+const send = document.querySelector(".contact_button");
+const closeForm = document.querySelector(".close");
+
+const firstName = document.querySelector("#first");
+const lastName = document.querySelector("#last");
+const email = document.querySelector("#email");
+const message = documment.querySelector("#message");
+
+
+
+// FUNCTIONS
+/**
+ * Manage the navigation page
+ */
+function openClosePageNavigation() {
+    var nodeNavigation = document.getElementById("pageNavigation");
+  
+    if (nodeNavigation.classList.contains("isNavigationOpen")) {
+        nodeNavigation.classList.remove("isNavigationOpen");
+    } else {
+        nodeNavigation.classList.add("isNavigationOpen");
+    }
+}
+  
+/**
+ * launch modal form
+ */
+function launchModal() {
+    modalbg.style.display = "block";
+
+    const myBody = document.querySelector('body');
+    myBody.classList.add('isModalOpen');
 }
 
+/**
+ * closing modal form
+ */
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+    modalbg.style.display = "none";
+
+    const openForm = document.querySelector('.modal');
+    openForm.classList.remove('hidden');
+
+    // const closeForm = document.querySelector('#closeForm');
+    // closeForm.classList.remove('active');
+
+    const myBody = document.querySelector('body');
+    myBody.classList.remove('isModalOpen');
 }
+
+// TO OPEN OR CLOSE THE FORM
+// launch modal event
+send.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// closing modal event
+closeForm.forEach((btn) => btn.addEventListener("click", closeModal));
+//btnCloseEnd.forEach((btn) => btn.addEventListener("click", closeModal));
+
+
+
+// CODE FOURNI
+// function displayModal() {
+//     const modal = document.getElementById("contact_modal");
+// 	modal.style.display = "block";
+// }
+
+// function closeModal() {
+//     const modal = document.getElementById("contact_modal");
+//     modal.style.display = "none";
+// }
